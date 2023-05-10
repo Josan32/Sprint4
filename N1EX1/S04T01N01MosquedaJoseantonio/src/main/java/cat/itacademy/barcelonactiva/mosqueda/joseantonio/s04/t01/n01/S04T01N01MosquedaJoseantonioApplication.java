fdd028283110ca7d448cfc8fe36a6bb81841f7ae
Jose Antonio <127103471+Josan32@users.noreply.github.com>
2023-05-10 09:14:40 +0200

    Add files via upload

---
+++ N1EX1/S04T01N01MosquedaJoseantonio/src/main/java/cat/itacademy/barcelonactiva/mosqueda/joseantonio/s04/t01/n01/S04T01N01MosquedaJoseantonioApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.mosqueda.joseantonio.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01MosquedaJoseantonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01MosquedaJoseantonioApplication.class, args);
+	}
+
+}
